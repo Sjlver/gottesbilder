43bc94f06ef7b4b9f241312c6009f2f9de3db2f4
Jonas Wagner <ltlygwayh@gmail.com>
2021-05-29 19:07:55 +0200

    Change build output to go to "docs/" in the main branch.
    That way, deployment gets a little easier (just "npm run build" and git
    push).

---
+++ docs/cards/cards.ts
@@ -0,0 +1,77 @@
+export const cards = {
+    "adler": {
+        "photo": "adler.jpg"
+    },
+    "arzt": {
+        "photo": "arzt.jpg"
+    },
+    "behueter": {
+        "photo": "behueter.jpg"
+    },
+    "burg": {
+        "photo": "burg.jpg"
+    },
+    "fels": {
+        "photo": "fels.jpg"
+    },
+    "feuer": {
+        "photo": "feuer.jpg"
+    },
+    "fluegel": {
+        "photo": "fluegel.jpg"
+    },
+    "freund": {
+        "photo": "freund.jpg"
+    },
+    "gaertner": {
+        "photo": "gaertner.jpg"
+    },
+    "hand": {
+        "photo": "hand.jpg"
+    },
+    "hebamme": {
+        "photo": "hebamme.jpg"
+    },
+    "henne": {
+        "photo": "henne.jpg"
+    },
+    "hirt": {
+        "photo": "hirt.jpg"
+    },
+    "licht": {
+        "photo": "licht.jpg"
+    },
+    "liebe": {
+        "photo": "liebe.jpg"
+    },
+    "loewe": {
+        "photo": "loewe.jpg"
+    },
+    "mutter": {
+        "photo": "mutter.jpg"
+    },
+    "quelle": {
+        "photo": "quelle.jpg"
+    },
+    "retter": {
+        "photo": "retter.jpg"
+    },
+    "richter": {
+        "photo": "richter.jpg"
+    },
+    "schoepfer": {
+        "photo": "schoepfer.jpg"
+    },
+    "sonne": {
+        "photo": "sonne.jpg"
+    },
+    "toepfer": {
+        "photo": "toepfer.jpg"
+    },
+    "vater": {
+        "photo": "vater.jpg"
+    },
+    "zelt": {
+        "photo": "zelt.jpg"
+    }
+}
